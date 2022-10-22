1f86028b038408cf57fc42bd493e681f33b81f7d
thatsnotamuffin <bretlrickerson@gmail.com>
2022-10-22 12:34:51 -0500

    Corrected kubectl download location when a version is specified

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,9 @@ if [ -z ${KUBECTL_VERSION} ] ; then
     echo "Using kubectl version: $(kubectl version --client --short)"
 else
     echo "Installing kubection version: $KUBECTL_VERSION"
-    rm /usr/bin/kubectl
-    curl -sL -o /usr/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/"$KUBECTL_VERSION"/bin/linux/amd64/kubectl && \
-    chmod +x /usr/bin/kubectl
+    rm /usr/local/bin/kubectl
+    curl -sL -o /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/"$KUBECTL_VERSION"/bin/linux/amd64/kubectl && \
+    chmod +x /usr/local/bin/kubectl
 fi
 
 sh -c "$*"
